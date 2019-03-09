c8c528948675e357eebe134746c6f3ee8c70a4cf
Ayoub Chouak <a.chouak@protonmail.com>
2019-03-09 14:21:02 +0100

    Add ConfigurationProviderSingleton, MqttConfig. Edit ClientMain.

---
+++ assets/ConfigurationProviderSingleton.java
@@ -0,0 +1,19 @@
+import org.cfg4j.provider.ConfigurationProvider;
+import org.cfg4j.provider.ConfigurationProviderBuilder;
+import org.cfg4j.source.ConfigurationSource;
+
+public class ConfigurationProviderSingleton
+{
+    private static ConfigurationProvider _cfgProvider;
+
+    public static ConfigurationProvider getProvider(ConfigurationSource src)
+    {
+        if (_cfgProvider == null) {
+            _cfgProvider = new ConfigurationProviderBuilder()
+                                .withConfigurationSource(src)
+                                .build();
+        }
+
+        return _cfgProvider;
+    }
+}
--- mqtt-client/src/ClientMain.java
+++ mqtt-client/src/ClientMain.java
@@ -1,42 +1,52 @@
 import javafx.util.Pair;
+import org.cfg4j.provider.ConfigurationProvider;
+import org.cfg4j.source.git.GitConfigurationSourceBuilder;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.logging.JSR47Logger;
 
 import java.io.Serializable;
 
-/**
- * @todo: Get config from Git repo
- *
- */
 public class ClientMain
 {
-    private static String _MQTT_BROKER = "tcp://localhost:1883";
+    private static String _GIT_CONFIG_URI = "https://github.com/ntauth/iot-citymon-cfg";
+    private static JSR47Logger _logger = new JSR47Logger();
 
     public static void main(String[] args)
     {
+        // Get the configuration provider
+        ConfigurationProvider cfgProvider = ConfigurationProviderSingleton
+                .getProvider(new GitConfigurationSourceBuilder()
+                                .withRepositoryURI(_GIT_CONFIG_URI)
+                                .build());
+        MqttConfig cfg = cfgProvider.bind("mqtt", MqttConfig.class);
+
         MqttConnectOptions opts = new MqttConnectOptions();
         opts.setCleanSession(true);
 
         try
         {
-            MqttClient client = MqttConnectionSingleton.getClient(_MQTT_BROKER, System.getenv("MQTT_CLIENT_ID"), opts);
+            MqttClient client = MqttConnectionSingleton.getClient(cfg.brokerUrl(), System.getenv("MQTT_CLIENT_ID"), opts);
             MqttQueue queue = new MqttQueue();
 
+            // Spawn worker thread
             Thread worker = new Thread(new MqttWorker(client, queue));
             worker.start();
 
-            while (true) {
+            // Ingestion loop
+            for (;;)
+            {
                 try {
                     queue.enqueue(new Pair<String, Serializable>("iot-citymon-test", "ACK"));
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    _logger.severe(ClientMain.class.getName(), "main", e.getMessage());
                 }
             }
-        } catch (MqttException e) {
-            // @todo: Log failure
-            e.printStackTrace();
+        }
+        catch (MqttException e) {
+            _logger.severe(ClientMain.class.getName(), "main", e.getMessage());
         }
     }
 }
